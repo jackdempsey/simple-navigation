166d6a7dc9dbaf7475328072b149e4bbc978ad30
Jack Dempsey <jack.dempsey@gmail.com>
2010-02-23 21:46:38 -0500

    fix deprecations -- rails/init is now init

--- init.rb
+++ init.rb
@@ -1,1 +1,3 @@
-require File.dirname(__FILE__) + "/rails/init"
+default_config_file_path = File.join(Rails.root, 'config')
+SimpleNavigation.config_file_path = default_config_file_path unless SimpleNavigation.config_file_path
+ActionController::Base.send(:include, SimpleNavigation::ControllerMethods)
--- rails/init.rb
+++
@@ -1,3 +0,0 @@
-default_config_file_path = File.join(Rails.root, 'config')
-SimpleNavigation.config_file_path = default_config_file_path unless SimpleNavigation.config_file_path
-ActionController::Base.send(:include, SimpleNavigation::ControllerMethods)
